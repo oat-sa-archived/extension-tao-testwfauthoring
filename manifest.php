a7f49e2257c27fb52a3f4d0726cfb5a495c93168
Joel Bout <joel@taotesting.com>
2014-09-16 10:28:04 +0200

    Removed constant TAO_BASEWWW

--- manifest.php
+++ manifest.php
@@ -69,8 +69,5 @@ return array(
 		#BASE WWW the web resources path
 		'BASE_WWW'				=> ROOT_URL	.'taoWfAdvTest/views/',
 	
-	 	#TAO extension Paths
-		'TAOBASE_WWW'			=> ROOT_URL	.'tao/views/',
-	
 	)
 );
