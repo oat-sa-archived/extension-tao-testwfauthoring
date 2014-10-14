5878f35423f82a16fe933dda3ec6f7a4f46b86ca
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2014-10-14 09:56:49 +0200

    dedicated compilation config

---
+++ views/build/grunt/bundle.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) { 
+
+    var requirejs   = grunt.config('requirejs') || {};
+    var clean       = grunt.config('clean') || {};
+    var copy        = grunt.config('copy') || {};
+
+    var root        = grunt.option('root');
+    var libs        = grunt.option('mainlibs');
+    var ext         = require(root + '/tao/views/build/tasks/helpers/extensions')(grunt, root);
+
+    /**
+     * Remove bundled and bundling files
+     */
+    clean.taowfadvtestbundle = ['output',  root + '/taoWfAdvTest/views/js/controllers.min.js'];
+    
+    /**
+     * Compile tao files into a bundle 
+     */
+    requirejs.taowfadvtestbundle = {
+        options: {
+            baseUrl : '../js',
+            dir : 'output',
+            mainConfigFile : './config/requirejs.build.js',
+            paths : { 'taoWfAdvTest' : root + '/taoWfAdvTest/views/js' },
+            modules : [{
+                name: 'taoWfAdvTest/controller/routes',
+                include : ext.getExtensionsControllers(['taoWfAdvTest']),
+                exclude : ['mathJax', 'mediaElement'].concat(libs)
+            }]
+        }
+    };
+
+    /**
+     * copy the bundles to the right place
+     */
+    copy.taowfadvtestbundle = {
+        files: [
+            { src: ['output/taoWfAdvTest/controller/routes.js'],  dest: root + '/taoWfAdvTest/views/js/controllers.min.js' },
+            { src: ['output/taoWfAdvTest/controller/routes.js.map'],  dest: root + '/taoWfAdvTest/views/js/controllers.min.js.map' }
+        ]
+    };
+
+    grunt.config('clean', clean);
+    grunt.config('requirejs', requirejs);
+    grunt.config('copy', copy);
+
+    // bundle task
+    grunt.registerTask('taowfadvtestbundle', ['clean:taowfadvtestbundle', 'requirejs:taowfadvtestbundle', 'copy:taowfadvtestbundle']);
+};
