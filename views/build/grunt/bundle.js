4d6dca3e176614d08c026e4d0d81eb8c673ade45
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2015-02-03 11:24:29 +0100

    fix parrallel build

--- views/build/grunt/bundle.js
+++ views/build/grunt/bundle.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
     /**
      * Remove bundled and bundling files
      */
-    clean.taowfadvtestbundle = [out,  root + '/taoWfAdvTest/views/js/controllers.min.js'];
+    clean.taowfadvtestbundle = [out];
     
     /**
      * Compile tao files into a bundle 
